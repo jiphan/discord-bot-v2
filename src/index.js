5a763305c05714ed86a0d0e542fcf63d7d131be6
jiphan <jiphan@umich.edu>
2020-11-05 17:45:09 -0600

    added aws, functions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js')
 const keys = require('./keys.json')
 const client = new Discord.Client()
 
+client.login(keys.discord_bot_token)
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
     if (msg.content === 'ping') msg.reply('pong!')
-    if (msg.content === 'marco') msg.reply('polo!')
-})
+    if (!msg.content.startsWith('!')) return
+    let args = msg.content.substring(1).split(' ')
 
-client.login(keys.discord_bot_token)
+})
